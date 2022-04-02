5fcf15524b7eb99c03baedb57df0055d9dea68dc
Yagnik Patel <coderisaddicted@gmail.com>
2022-04-02 16:35:30 +0530

    react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import MuiThemeWrap from "./components/MuiThemeWrap";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <MuiThemeWrap />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
